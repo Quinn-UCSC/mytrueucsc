cacef4542c9ad0e5874f9eba95357595af0e59ff
Quinn-UCSC <migquinn@ucsc.edu>
2023-06-10 00:28:48 -0700

    New tab6 (Housing) + updated youtube API

--- youtube-api.js
+++ youtube-api.js
@@ -12,8 +12,25 @@ $.ajax({
         // Variable to store the specific items of the third playlist in the data index
         var housePlaylist = data.items[3];
         console.log(housePlaylist);
-        // Replaces div id output with an iframe that uses the id data of the playlist as the youtube id url
-        $("#output").append("<iframe width='500' height='300' class='yt-frame' src='https://www.youtube.com/playlist?list="+housePlaylist.id+"'>");
+        // Create a div to contain the player
+        var playerDiv = $("<div>").attr("id", "player");
+        $("#output").append(playerDiv);
+
+        // Load the YouTube IFrame Player API
+        $.getScript("https://www.youtube.com/iframe_api", function() {
+            // Function called when YouTube IFrame Player API is ready
+            window.onYouTubeIframeAPIReady = function () {
+                // Create the player
+                new YT.Player("player", {
+                    height: "300",
+                    width: "500",
+                    playerVars: {
+                        listType: "playlist",
+                        list: housePlaylist.id,
+                    },
+                });
+            };
+        });
     },
     // Check to see if there is an error in API
     error: function (jqXHR, textStatus, errorThrown) {
