045e6db4821511a466f4fab26d8b90290d91edab
Quinn-UCSC <migquinn@ucsc.edu>
2023-06-09 19:35:07 -0700

    Updated studenthub page + displaynews code
    -Made the heading look more appealing for the studenthub tutorial
    -displayNews function repeats more fluidly and actually continues to repeat infinitely instead of randomly stopping

--- displaynews.js
+++ displaynews.js
@@ -4,31 +4,32 @@
 
 // Assuming you have an array of news stored in the `newsArray` variable
 var newsArray = [
-    "Did you know that you could join discord servers at the UCSC Discord Student Hub? Please check out the student hub tutorial <a href='studenthub.html'>here</a>!",
-    "Please come to ARTG-80I's new Playfaire <span style = 'font-style: italic;'>happening</span> <span style = 'font-weight: bold;'>NEXT Wednesday</span>! I promise you will have a good time!",
-  ];
-  
-  function displayNews() {
-    var newsContainer = $('#news-header');
-    var newsContent = '';
+  "Did you know that you could join discord servers at the UCSC Discord Student Hub? Please check out the student hub tutorial <a href='studenthub.html'>here</a>!",
+  "Please come to ARTG-80I's new Playfaire <span style='font-style: italic;'>happening</span> <span style='font-weight: bold;'>NEXT Wednesday</span>! I promise you will have a good time!"
+];
 
-    // Repeat the newsArray multiple times to create a continuous loop
-    var repeatedArray = newsArray.concat(newsArray);
-  
-    // Loop through the repeatedArray and create the content for the scrolling header
-    for (var i = 0; i < repeatedArray.length; i++) {
-        newsContent += '<span class="news-item">' + repeatedArray[i] + '</span>';
-  
+function displayNews() {
+  var newsContainer = $('#news-header');
+  var newsContent = '';
+
+  // Loop through the newsArray and create the content for the scrolling header
+  for (var i = 0; i < newsArray.length; i++) {
+    newsContent += '<span class="news-item">' + newsArray[i] + '</span>';
+
     // If it's not the last news item, add a notification for the next news
     if (i !== newsArray.length - 1) {
-        newsContent += ' <span class="notification" style = "10px;">Next-up: </span>';
-      }
+      newsContent += ' <span class="notification">Next-up: </span>';
     }
-  
-    newsContainer.html(newsContent);
   }
-  
-  // Call the displayNews function to start displaying the news
-  $(document).ready(function() {
-    displayNews();
-  });  
+  newsContainer.html(newsContent);
+
+  // Restart the displayNews function once the news array is finished
+  if (i === newsArray.length) {
+    setTimeout(displayNews, 45000); // Restart after a 45-second delay (adjust as needed)
+  }
+}
+
+// Call the displayNews function to start displaying the news
+$(document).ready(function() {
+  displayNews();
+});
