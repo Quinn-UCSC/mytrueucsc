e17fce07bbcd0af02fc5f74a63d6d90a6461d084
Quinn-UCSC <migquinn@ucsc.edu>
2023-06-20 23:00:18 -0700

    Updated featurednews.js

--- js/featurednews.js
+++ js/featurednews.js
@@ -3,72 +3,66 @@
 // Date: 6/20/2023
 
 $(document).ready(function() {
-    const slideshowContainer = $('.slideshow');
-    const slideshowParagraph = slideshowContainer.find('p');
+  var url = "https://news.ucsc.edu/";
 
-    // Function to transition to the next item
-    function nextItem(items, currentIndex) {
-      // Update the text content with the current item
-      slideshowParagraph.html(items[currentIndex]);
+  // Make AJAX request
+  $.ajax({
+    url: "https://cors-anywhere.herokuapp.com/https://news.ucsc.edu/",
+    dataType: "html",
+    success: function(data) {
+      var html = $(data);
 
-      // Remove the "active" class from the current item
-      slideshowParagraph.removeClass('active');
+      // Find the content box element
+      var contentBox = html.find(".content-box");
 
-      // Increment the current index
-      currentIndex = (currentIndex + 1) % items.length;
+      // Find all the list items within the content box
+      var listItems = contentBox.find("li");
 
-      // Add the "active" class to the next item
-      slideshowParagraph.addClass('active');
+      // Set initial index and display the first item
+      var currentIndex = 0;
+      displayItem();
 
-      // Transition to the next item every 10 seconds
-      setTimeout(function() {
-        nextItem(items, currentIndex);
-      }, 10000);
-    }
+      // Function to display each item
+      function displayItem() {
+        // Get the current list item
+        var currentItem = $(listItems[currentIndex]);
 
-    // Fetch data from the URL using AJAX
-    $.ajax({
-      url: 'https://cors-anywhere.herokuapp.com/https://news.ucsc.edu/',
-      method: 'GET',
-      dataType: 'html',
-      success: function(data) {
-        const items = [];
+        // Extract the necessary information
+        var type = url + currentItem.find(".type").text().trim();
+        var link = url + currentItem.find("a").attr("href");
+        var image = url + currentItem.find("img").attr("src");
+        var title = url + currentItem.find("h2 a").text().trim();
+        var description = url + currentItem.find("p").text().trim();
 
-        // Extract the necessary information from each <li> item in the .content-box section
-        $(data).find('.content-box li').each(function() {
-          const listItem = $(this);
-          const type = listItem.find('.type').text().trim();
-          const link = listItem.find('a').attr('href');
-          const image = listItem.find('img').attr('src');
-          const title = listItem.find('h2 a').text().trim();
-          const description = listItem.find('p').text().trim();
+        // Create the HTML for the item
+        var itemHTML = "<div class='item'>" +
+          "<p>" + type + "</p>" +
+          "<a href='" + link + "'>" +
+          "<img src='" + image + "' alt='" + title + "'>" +
+          "<h2>" + title + "</h2>" +
+          "<p>" + description + "</p>" +
+          "</a>" +
+          "</div>";
 
-          // Create an HTML structure to display the information
-          const itemHTML = `
-            <div>
-              <h2>${type}</h2>
-              <a href="${link}">
-                <img src="${image}" alt="${title}">
-              </a>
-              <h3>${title}</h3>
-              <p>${description}</p>
-            </div>
-          `;
+        // Fade out the current item
+        $(".slideshow").fadeOut(500, function() {
+          // Replace the content with the new item HTML
+          $(this).html(itemHTML);
 
-          items.push(itemHTML);
+          // Fade in the new item
+          $(this).fadeIn(500);
         });
 
-        if (items.length > 0) {
-          let currentIndex = 0;
-          nextItem(items, currentIndex);
-        } else {
-          // Display an error message if no items were found
-          slideshowParagraph.text('No items found');
-        }
-      },
-      error: function(xhr, status, error) {
-        // Display the error message in the HTML page
-        slideshowParagraph.text('AJAX request failed: ' + error);
+        // Increment the index or reset to 0 if reached the end
+        currentIndex = (currentIndex + 1) % listItems.length;
+
+        // Call the displayItem function again after 10 seconds
+        setTimeout(displayItem, 10000);
       }
-    });
+    },
+    error: function(xhr, textStatus, errorThrown) {
+      // Display error message
+      $(".slideshow").html("<p>AJAX request failed: " + textStatus + "</p>");
+    }
   });
+});
